e0a8268f362f6b582d61d8407659768848422848
Nagaraj0229 <npnagu50@gmail.com>
2023-10-03 18:44:08 +0530

    chore: fix the error

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,14 @@ import { StudentSchema } from './schema/student';
 import { StudentService } from './students/student.service';
 import { StudentsModule } from './students/students.module';
 import { AppController } from './app.controller';
-import { SchoolModule } from './school/school.module';
-import { SchoolController } from './no-spec/school/school.controller';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://nagaraj03:Nabi0229@amwhiz.ozlipso.mongodb.net/'),
     MongooseModule.forFeature([{ name: 'School/Student', schema: StudentSchema }]),
     StudentsModule,
-    SchoolModule,
   ],
-  controllers: [StudentController, AppController, SchoolController],
+  controllers: [StudentController, AppController],
   providers: [StudentService],
 })
 export class AppModule {}
