262eb988af5f20c67fafaa88007bdafd28a7f731
Nagaraj0229 <npnagu50@gmail.com>
2023-12-19 14:14:12 +0530

    src: test and modify loggers messsages and middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
 import { MongooseModule } from '@nestjs/mongoose';
-import { StudentController } from './students/student.controller';
+import StudentController from './students/student.controller';
 import { StudentSchema } from './schema/student';
 import StudentService from './students/student.service';
 import { StudentsModule } from './students/students.module';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -16,7 +15,7 @@ import { AppController } from './app.controller';
     MongooseModule.forFeature([{ name: 'School/Student', schema: StudentSchema }]),
     StudentsModule,
   ],
-  controllers: [StudentController, AppController],
+  controllers: [StudentController],
   providers: [StudentService],
 })
 export class AppModule {}
